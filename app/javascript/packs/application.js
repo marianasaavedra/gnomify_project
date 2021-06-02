daa6f7204b5f779dbc6845b81d2e7dbdd30ce8b7
mariana saavedra <marianadsbs@gmail.com>
2021-06-02 16:13:40 +0100

    Fixing calender

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,14 +29,15 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initFlatpickr } from "../plugins/flatpickr";
 import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
+  initFlatpickr();
   // Call your functions here, e.g:
   // initSelect2();
 });
 
-import { initFlatpickr } from "../plugins/flatpickr";
 
-initFlatpickr();
+
