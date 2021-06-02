7fa9dbe2325865ff5f3cc1dd06ca3ba6b585fcf8
mariana saavedra <marianadsbs@gmail.com>
2021-06-02 14:44:56 +0100

    My Bookings page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+
   def my_bookings
+    @bookings = Booking.where(user: current_user)
   end
+
   def home
   end
 end
